8223c3c1c2380d615815a62482a6c24d9ee41d7c
CosmicOrder <Matrixxturn@yandex.ru>
2022-06-13 15:41:46 +0300

    refactor: update code format, add related_name for models's field

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,16 +3,16 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField("Название на русском", max_length=200)
-    title_en = models.CharField("Название на анлийском", max_length=200, \
-                                blank=True)
-    title_jp = models.CharField("Название на японском", max_length=200, \
-                                blank=True)
+    title_en = models.CharField("Название на анлийском", max_length=200, blank=True)
+    title_jp = models.CharField("Название на японском", max_length=200, blank=True)
     image = models.ImageField("Изображение", blank=True)
     description = models.TextField("Описание", blank=True)
-    previous_evolution = models.ForeignKey("Pokemon",
-                                           on_delete=models.SET_NULL,
-                                           null=True,
-                                           related_name="next_evolutions")
+    previous_evolution = models.ForeignKey(
+        "Pokemon",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="next_evolutions",
+    )
 
     def __str__(self):
         return self.title
@@ -20,16 +20,16 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE,
-                                verbose_name='Покемон')
+                                verbose_name='Покемон', related_name='pokemon')
     lat = models.FloatField()
     lon = models.FloatField()
     appeared_at = models.DateTimeField("Появится", null=True, blank=True)
     disappeared_at = models.DateTimeField("Исчезнет", null=True, blank=True)
     level = models.IntegerField("Уровень", null=True, blank=True)
-    Health = models.IntegerField("Здоровье", null=True, blank=True)
-    Strength = models.IntegerField("Сила", null=True, blank=True)
-    Defence = models.IntegerField("Защита", null=True, blank=True)
-    Stamina = models.IntegerField("Выносливость", null=True, blank=True)
+    health = models.IntegerField("Здоровье", null=True, blank=True)
+    strength = models.IntegerField("Сила", null=True, blank=True)
+    defence = models.IntegerField("Защита", null=True, blank=True)
+    stamina = models.IntegerField("Выносливость", null=True, blank=True)
 
     def __str__(self):
         return self.pokemon.title
